57bad601a3f1663c3935015d3060804c417b920a
Elena Mihaela Ignat <elen1068@student.eadania.dk>
2018-05-01 13:47:24 +0200

    model+vm

---
+++ FoxtrotProject/Model/Customer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxtrotProject.Model
+{
+    class Customer
+    {
+        private int Cvr;
+
+        public int cvr
+        {
+            get { return cvr; }
+            set { cvr = value; }
+        }
+
+
+
+        private string Name;
+
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        private string Address;
+
+        public string address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+
+        private int TelefonNr;
+
+        public int telefonNr
+        {
+            get { return telefonNr; }
+            set { telefonNr = value; }
+        }
+
+
+        private string CustomerInfo;
+
+        public string customerInfo
+        {
+            get { return customerInfo; }
+            set { customerInfo = value; }
+        }
+
+
+        private string ContactPerson;
+
+        public string contactPerson
+        {
+            get { return contactPerson; }
+            set { contactPerson = value; }
+        }
+
+
+        private int GrossIncome;
+
+        public int grossIncome
+        {
+            get { return grossIncome; }
+            set { grossIncome = value; }
+        }
+
+    }
+}
---
+++ FoxtrotProject/ViewModel/ViewModelCustomer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ViewModelCustomer : INotifyPropertyChanged
+{
+    private Customer _cust = new Customer();
+    public Customer Cust
+    {
+        get { return _cust; }
+        set { _cust = value; }
+    }
+
+    public string TextName
+    {
+        get { return _cust.name; }
+        set { _cust.name = value; NotifyPropertyChanged(); }
+    }
+
+    public string TextAddress
+    {
+        get { return _cust.address; }
+        set { _cust.address = value; NotifyPropertyChanged(); }
+    }
+
+    public int TextTelefonNr
+    {
+        get { return _cust.telefonNr; }
+        set { _cust.telefonNr = value; NotifyPropertyChanged(); }
+    }
+
+    public string TextCustomerInfo
+    {
+        get { return _cust.customerInfo; }
+        set { _cust.customerInfo = value; NotifyPropertyChanged(); }
+    }
+
+    public string TextContactPerson
+    {
+        get { return _cust.contactPerson; }
+        set { _cust.contactPerson = value; NotifyPropertyChanged(); }
+    }
+    
+    public int TextGrossIncome
+    {
+        get { return _cust.grossIncome; }
+        set { _cust.grossIncome = value; NotifyPropertyChanged(); }
+    }
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        //PropertyChanged?.Invoke(this. new P)
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
