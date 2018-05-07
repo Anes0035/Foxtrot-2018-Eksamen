283ac1f8a00d5ce3d08d526e774e4db711d2d45d
Kasper Bach Pedersen <kasp9786@student.eadania.dk>
2018-05-07 11:04:42 +0200

    Database connection / metoder

--- FoxtrotProject/Model/Database.cs
+++ FoxtrotProject/Model/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoxtrotProject.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -13,12 +14,87 @@ namespace FoxtrotProject.Model
 
         private void OpenConnection()
         {
-            throw new NotImplementedException();
+            SqlConnection connection = new SqlConnection("Data Source =.; Initial Catalog = Foxtrot_SQLProject; Integrated Security = True");
+            connection.Open();
         }
 
         private void CloseConnection()
         {
-            throw new NotImplementedException();
+            connection.Close();
+        }
+
+        /// Method for Adding a customer
+
+        public void AddCustomer(Customer customer)
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand command = new SqlCommand("Insert INTO Customer(CVR, Name, Address, PhoneNumber, ContactPerson) Values(@cvr, @name, @address, @phonenumber, @contactperson, @SetDate)", connection);
+                command.Parameters.AddWithValue("@cvr", customer.CVR);
+                command.Parameters.AddWithValue("@name", customer.Name);
+                command.Parameters.AddWithValue("@address", customer.Address);
+                command.Parameters.AddWithValue("@phonenumber", customer.TelephoneNumber);
+                command.Parameters.AddWithValue("@contactperson", customer.ContactPerson);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                // Create an Exception if Customer already exists
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /// Method for editing a customers details ( Not Finished ) 
+
+        public void EditCustomer(Customer customer)
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand command = new SqlCommand("UPDATE Customer SET CVR = @cvr, Name = @name, Address = @address, PhoneNumber = @phonenumber, ContactPerson = @contactperson ");
+                command.Parameters.AddWithValue("@cvr", customer.CVR);
+                command.Parameters.AddWithValue("@name", customer.Name);
+                command.Parameters.AddWithValue("@address", customer.Address);
+                command.Parameters.AddWithValue("@phonenumber", customer.TelephoneNumber);
+                command.Parameters.AddWithValue("@contactperson", customer.ContactPerson);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                // Create an Exception if customer isnt in the database
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        ///  Method for removing a customer
+
+        public void RemoveCustomer()
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand command = new SqlCommand("DELETE FROM Customer", connection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                // Create an exception if no Customer is selected
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
         }
     }
 }
