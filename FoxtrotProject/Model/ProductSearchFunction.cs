39d3ec7a2f1455f311d2abff3557695da31f0346
Anes Salak <anes0035@student.eadania.dk>
2018-05-16 12:42:06 +0200

    Product search functionality

---
+++ FoxtrotProject/Model/ProductSearchFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxtrotProject.Model
+{
+    class ProductSearchFunction
+    {
+        public ObservableCollection<Product> SearchProductName(ObservableCollection<Product> products, string searchString)
+        {
+            return (ObservableCollection<Product>) products.Where(p => p.Name.Contains(searchString));
+        }
+        public ObservableCollection<Product> SearchProductDescription(ObservableCollection<Product> products, string searchString)
+        {
+            return (ObservableCollection<Product>)products.Where(p => p.Description.Contains(searchString));
+        }
+        public ObservableCollection<Product> SearchProductID(ObservableCollection<Product> products, string searchString)
+        {
+            return (ObservableCollection<Product>)products.Where(p => p.ID.ToString().Contains(searchString));
+        }
+    }
+}
