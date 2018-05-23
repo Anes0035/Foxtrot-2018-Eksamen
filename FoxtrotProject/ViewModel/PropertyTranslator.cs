309c774e9338bdaad03f5e8e539dcce90c0951ec
Christian Terp Jensen <chri329k@student.eadania.dk>
2018-05-23 12:28:05 +0200

    Update Validation, Property Translation

---
+++ FoxtrotProject/ViewModel/PropertyTranslator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxtrotProject.ViewModel
+{
+    class PropertyTranslator
+    {
+        private Dictionary<string, string> Translations;
+
+        public PropertyTranslator()
+        {
+            Translations = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            TranslationList();
+        }
+
+        private void TranslationList()
+        {
+            Translations.Add("Name", "Navn");
+            Translations.Add("Address", "Adresse");
+            Translations.Add("TelephoneNumber", "Tlf. nummer");
+            Translations.Add("ContactPerson", "Kontakt person");
+            Translations.Add("GrossIncome", "Årlig omsætning");
+        }
+
+        /// <summary>
+        /// Get english property name translated to danish
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns>Default return string is the parameter propertyName</returns>
+        public string GetTranslation(string propertyName)
+        {
+            if (Translations.ContainsKey(propertyName))
+                return Translations[propertyName];
+            else
+                return propertyName;
+        }
+    }
+}
