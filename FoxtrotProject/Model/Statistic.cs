1f26e36d40f0327a7ac4ef6734d494df120d723b
Anes Salak <anes0035@student.eadania.dk>
2018-05-28 11:23:08 +0200

    stat

--- FoxtrotProject/Model/Statistic.cs
+++ FoxtrotProject/Model/Statistic.cs
@@ -8,8 +8,34 @@ namespace FoxtrotProject.Model
 {
     class Statistic
     {
+        private Dictionary<ProductGroup, int> ProductGroupCount;
 
+        private ContractManager contractManager;
 
+        public Statistic(ContractManager contractManager)
+        {
+            this.contractManager = contractManager;
+            ProductGroupCount = new Dictionary<ProductGroup, int>();
+            contractManager.onAddContract += new EventHandler<ContractEventArgs>(CountProductGroup);
+        }
+
+        private void CountProductGroup(object sender, ContractEventArgs e)
+        {
+            foreach (ProductGroup pg in e.contract.ProductGroups)
+            {
+                if (!ProductGroupCount.ContainsKey(pg))
+                    ProductGroupCount.Add(pg, 1);
+                else
+                    ProductGroupCount[pg]++;
+            }
+            //Sort method Anes 
+            ProductGroupCount = GetSortedDictionary(ProductGroupCount);
+        }
+
+        private Dictionary<ProductGroup, int> GetSortedDictionary(Dictionary<ProductGroup, int> dictionary)
+        {
+            return (Dictionary<ProductGroup, int>) from pair in dictionary orderby pair.Value descending select pair;
+        }
 
         public List<Product> FindTopXProducts(int x, List<Contract> contracts)
         {
@@ -17,3 +43,4 @@ namespace FoxtrotProject.Model
         }
     }
 }
+
